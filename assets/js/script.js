f3dee91b75e3cae9d240cb92bf5585030847aac6
Axelhrdz <axelhernandezperez23@gmail.com>
2024-03-11 18:12:20 -0600

    adding some sections

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// Adding funcitonality to the hamburger navabr button
+var navbarLinks = document.getElementById('navLinks')
+var hamburger = document.getElementById('ham-button')
+
+hamburger.onclick = function(){
+    navbarLinks.classList.toggle('show-nav')
+}
+// Adding funcitonality to the hamburger navabr button
+
+
