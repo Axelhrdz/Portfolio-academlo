8443eacbe9f75b72859b33656ece34a6c819bc8c
Axelhrdz <axelhernandezperez23@gmail.com>
2024-03-13 14:51:54 -0600

    adding sections and some styling changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,11 @@ hamburger.onclick = function(){
 }
 // Adding funcitonality to the hamburger navabr button
 
+//Scroll reveal
+ScrollReveal().reveal('.header', {delay: 300});
+ScrollReveal().reveal('.aboutMe', {delay: 300});
+ScrollReveal().reveal('.experience', {delay: 300});
+ScrollReveal().reveal('.stack', {delay: 300});
+
+
 
