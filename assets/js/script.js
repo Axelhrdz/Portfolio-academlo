de9c6733fb6390269e7bccc2ced1d5158390104c
Axelhrdz <axelhernandezperez23@gmail.com>
2024-03-13 18:42:34 -0600

    adding reviews section, and failed slider with swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+
 // Adding funcitonality to the hamburger navabr button
 var navbarLinks = document.getElementById('navLinks')
 var hamburger = document.getElementById('ham-button')
@@ -12,6 +14,8 @@ ScrollReveal().reveal('.header', {delay: 300});
 ScrollReveal().reveal('.aboutMe', {delay: 300});
 ScrollReveal().reveal('.experience', {delay: 300});
 ScrollReveal().reveal('.stack', {delay: 300});
+ScrollReveal().reveal('.reviewsContainer', {delay: 300});
+
 
 
 
